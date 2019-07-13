052db235f221c57007b11938784319afe3632213
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-13 11:36:19 -0400

    added firebase conif link

---
+++ assets/javascript/schedule.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyA7Q2rAgvjZzzyrVk9aqVHjuotl1AwTDgs",
+  authDomain: "hawks-app-d7bf9.firebaseapp.com",
+  databaseURL: "https://hawks-app-d7bf9.firebaseio.com",
+  projectId: "hawks-app-d7bf9",
+  storageBucket: "hawks-app-d7bf9.appspot.com",
+  messagingSenderId: "47736646938",
+  appId: "1:47736646938:web:db46e68dfe633622"
+};
+
+firebaseConfig.initializeApp(config);
